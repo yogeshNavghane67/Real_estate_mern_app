37409625db71b9cd92e36db950fe559d5f4aa98a
yogeshNavghane67 <yogeshnavghane661@gmail.com>
2023-10-13 15:48:30 -0700

    create and run the server

--- api/index.js
+++ api/index.js
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 
 app.listen(3000, () => {
-    console.log('sever is running on port 3000');
+    console.log('server is running on port 3000');
 }
 );
